9f27fa1e2968ca938aaecfe1d3b05683002b25a7
roedyrustam <roedyrustam.id@gmail.com>
2022-05-19 07:55:33 +0800

    master

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -232,7 +232,7 @@
             <div class="container">
                 <div class="row align-items-center flex-row-reverse">
                     <div class="col-md-12 col-sm-12 text-center">
-                        Copyright © 2022 <a href="javascript:void(0)">Pandu Talenta Desa</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0)"> Roedy Rustam </a> All rights reserved.
+                        Copyright © 2022 <a href="javascript:void(0)">Pandu Talenta Digital</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0)"> Roedy Rustam </a> All rights reserved.
                     </div>
                 </div>
             </div>
